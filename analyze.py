6e53887d534bd3603e6caf2fb94a1e8e6b0793a2
Heather Guarnera <hguarnera@wooster.edu>
2023-08-29 08:47:47 -0400

    Initialize project skeleton

---
+++ analyze.py
@@ -0,0 +1,20 @@
+# load graph(s)
+
+# run analysis (subroutines)
+# - no. of nodes
+# - no. of edges
+# - hyperbolicity
+# - interval thinness (leanness)
+# - triangle thinness (insize)
+# - slimness
+# - cluster diameter
+# - value of i such that g is alpha-i metric graph
+# - helly gap
+# - tree length
+# - tree breadth
+# - tree width
+# - cop win number??
+# - various centrality measures (betweenness, closeness, etc....)
+
+
+# print results
