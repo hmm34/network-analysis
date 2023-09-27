04e7af76355f16bd05e4661f452884d35296b1b6
Heather Guarnera <hguarnera@wooster.edu>
2023-09-27 10:48:43 -0400

    Basic functionality
    * Load graph
    * Compute basic parameters radius, diameter, size, etc.
    * Compute hyperbolicity

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # network-analysis
+
+A framework to analyze a variety of graph parameters for a given network.
+
+To execute, run `sage -python analyze.py`
+
+
+# Dependencies
+
+- [matplotlib](https://matplotlib.org/stable/users/installing/index.html)
+- [sagemath](https://doc.sagemath.org/html/en/installation/index.html) pre-built binaries.
+- [networkX](https://networkx.org/documentation/stable/install.html)
+
+<!--
+
+Run `source venv/bin/activate` and then `jupyter notebook`.
+
+- [scipy](https://pypi.org/project/scipy/)
+- [grapht-tool](https://graph-tool.skewed.de/static/doc/index.html). Note: put it in the correct package directory in virtual environment, as it cannot be installed through pip. See [here](https://stackoverflow.com/questions/49222857/modulenotfounderror-no-module-named-graph-tool)
+-->
--- analyze.py
+++ analyze.py
@@ -1,20 +1,93 @@
-# load graph(s)
-
-# run analysis (subroutines)
-# - no. of nodes
-# - no. of edges
-# - hyperbolicity
-# - interval thinness (leanness)
-# - triangle thinness (insize)
-# - slimness
-# - cluster diameter
-# - value of i such that g is alpha-i metric graph
-# - helly gap
-# - tree length
-# - tree breadth
-# - tree width
-# - cop win number??
-# - various centrality measures (betweenness, closeness, etc....)
+# libriary imports
+import networkx as nx
+#from pyvis.network import Network
+import matplotlib.pyplot as plt
+from sage.graphs.hyperbolicity import hyperbolicity
+from sage.graphs.graph_input import from_networkx_graph
+from sage.all import *
 
 
-# print results
+# Given a sagemath graph, compute the interval thinness (leanness) using AO Mohammed et al approach
+# input: precomputed distance matrix D
+#        sagemath graph G
+#        list Q of all vertex pairs {x,y} of G sorted in nonn-increasing order with respect to d(x,y)
+# output: lambda, the leanness of G
+def compute_leanness(G):
+    return 0
+
+
+def compute_alpha_i_metric(G):
+    return 0
+
+
+def compute_pseudoconvexity(G):
+    return 0
+
+
+def compute_helly_gap(G):
+    return 0
+
+
+# aka insize
+def compute_triangle_thinness(G):
+    return 0
+
+
+def compute_slimness(G):
+    return 0
+
+
+def analyze(fileName):
+    ######################################
+    print("Loading graph" + fileName)
+
+    # load through networkx (lowercase g) -- networkx provides some functionality
+    filehandle = open(fileName, "rb")
+    g = nx.read_edgelist(filehandle, nodetype=int)
+    filehandle.close()
+
+    # load to sagemath (capital G) -- sagemath provides different functionality, e.g., hyperbolicity, viewing
+    G = Graph()
+    from_networkx_graph(G, g)
+
+    ######################################
+    # display graph
+    G.show(method="js", vertex_labels=True, edge_labels=False,         # optional - internet, needs sage.plot
+       link_distance=200, gravity=.05, charge=-500,
+       edge_partition=[[("11", "12", "2"), ("21", "21", "a")]],
+       edge_thickness=4)
+    #nx.draw(g)
+    plt.draw()
+    plt.show()
+
+    ######################################
+    # run analysis (subroutines)
+    print("Number of nodes n:", g.number_of_nodes())
+    print("Number of edges m:", g.number_of_edges())
+    print("Diameter:", nx.diameter(g))
+    print("Radius:", nx.radius(g))
+
+    ######################################
+    # - hyperbolicity
+    L,C,U = hyperbolicity(G, algorithm='BCCM');
+    print("Hyperbolicity: " + str(L))
+    
+
+    # - cluster diameter
+    # - tree length
+    # - tree breadth
+    # - tree width
+    # - cop win number??
+    # - various centrality measures (betweenness, closeness, etc....)
+
+    print("Degree Centrality Data")
+    print(nx.degree_centrality(g))
+
+
+    print("ok")
+
+
+
+# load graphs
+fileName = "data/small.txt"
+analyze(fileName)
